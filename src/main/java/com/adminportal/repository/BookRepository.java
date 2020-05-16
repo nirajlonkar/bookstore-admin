9555073d7200e3b00ec55370b298033c2d9cb718
Niraj Lonkar <Niraj Lonkar>
2020-05-16 20:48:18 +0530

    admin portal bookstore 1st push

---
+++ src/main/java/com/adminportal/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.adminportal.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.adminportal.domain.Book;
+
+public interface BookRepository extends CrudRepository<Book, Integer> {
+
+	
+}
---
+++ src/main/java/com/adminportal/service/impl/UserServiceImpl.java
@@ -0,0 +1,54 @@
+package com.adminportal.service.impl;
+
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.adminportal.domain.User;
+import com.adminportal.domain.security.UserRole;
+import com.adminportal.repository.RoleRepository;
+import com.adminportal.repository.UserRepository;
+import com.adminportal.service.UserService;
+ 
+
+@Service
+public class UserServiceImpl implements UserService{
+
+	private static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
+
+
+	@Override
+	public User createUser(User user, Set<UserRole> userRoles) throws Exception {
+		User localeUser = userRepository.findByUsername(user.getUsername());
+		if(localeUser != null) {
+			LOG.info("User {} already exists. Nothing will be done.", user.getUsername());
+			
+		}else {
+			for( UserRole ur : userRoles) {
+				roleRepository.save(ur.getRole());
+			}
+			
+			user.getUserRoles().addAll(userRoles);
+			localeUser = userRepository.save(user);
+		}
+		
+		
+		return localeUser;
+	}
+
+	@Override
+	public User save(User user) {
+		return userRepository.save(user);
+	}
+
+	
+}
